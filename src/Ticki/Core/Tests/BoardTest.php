ce336353c1c070849f17f78b5bedacd38ffc9968
Yevgenii Kolakovskiy <gtvarp@gmail.com>
2015-07-02 07:08:30 +0300

    Test board

--- src/Ticki/Core/Tests/BoardTest.php
+++ src/Ticki/Core/Tests/BoardTest.php
@@ -3,14 +3,74 @@
 namespace Ticki\Core\Tests;
 
 use Ticki\Core\Model\Board;
+use Ticki\Core\Model\Cell;
 
 class BoardClass extends \PHPUnit_Framework_TestCase
 {
-    public function testSuccessCreateBoard()
+	/**
+	 * @param $p1
+	 * @param $p2
+	 * @param $p3
+	 * @param $type
+	 *
+	 * @dataProvider horizontal
+	 */
+    public function testSuccessBoardFinishWin($p1, $p2, $p3, $type)
     {
         $board = new Board(3);
+	    $this->assertTrue(!$board->isFinish());
 
-        $this->assertEquals(3, $board->getSideCount());
+	    $board->addCell((new Cell($p1, $type)));
+	    $board->addCell((new Cell($p2, $type)));
+	    $board->addCell((new Cell($p3, $type)));
+
+		$this->assertTrue($board->isFinish());
+        $this->assertEquals($type, $board->getWinnerType());
     }
 
+	public function horizontal()
+	{
+		return array(
+			// horizontal
+			array(1, 2, 3, Cell::TAC),
+			array(4, 5, 6, Cell::TIC),
+			array(7, 8, 9, Cell::TAC),
+
+			// bisector
+			array(1, 5, 9, Cell::TAC),
+			array(3, 5, 7, Cell::TAC),
+
+			// vertical
+			array(1, 4, 7, Cell::TAC),
+			array(2, 5, 8, Cell::TAC),
+			array(3, 6, 9, Cell::TAC),
+
+		);
+	}
+
+	/**
+	 * o   x   o
+	 * x   o   x
+	 * x   o   x
+	 */
+	public function testSuccessBoardFinishNoWinner()
+	{
+		$board = new Board(3);
+		$type1 = Cell::TAC;
+		$type2 = Cell::TIC;
+		$this->assertTrue(!$board->isFinish());
+
+		$board->addCell((new Cell(1, $type1)));
+		$board->addCell((new Cell(2, $type2)));
+		$board->addCell((new Cell(3, $type1)));
+		$board->addCell((new Cell(4, $type2)));
+		$board->addCell((new Cell(5, $type1)));
+		$board->addCell((new Cell(6, $type2)));
+		$board->addCell((new Cell(7, $type2)));
+		$board->addCell((new Cell(8, $type1)));
+		$board->addCell((new Cell(9, $type2)));
+
+		$this->assertTrue($board->isFinish());
+		$this->assertEquals(null, $board->getWinnerType());
+	}
 }
